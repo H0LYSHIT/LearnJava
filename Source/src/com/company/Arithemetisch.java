18403a966e22ad2d42d9dcd8d69885fe340d9fa8
Pierre Michelsen <crakxhd@gmail.com>
2018-09-18 18:06:53 +0200

    Übung

---
+++ Source/src/com/company/Arithemetisch.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class Arithemetisch {
+
+    public static void main (String [] args){
+
+        int swag = 10;
+
+        int yolo = 5;
+
+        int ergebnis;
+
+        ergebnis = swag + yolo;
+
+        System.out.print("Das Ergebnis von swag + yolo ist =");
+        System.out.println(ergebnis);
+
+        ergebnis = swag - yolo;
+
+        System.out.print("Das Ergebnis von swag - yolo ist =");
+        System.out.println(ergebnis);
+
+        ergebnis = swag * yolo;
+
+        System.out.print("Das Ergebnis von swag * yolo ist =");
+        System.out.println(ergebnis);
+
+        ergebnis = swag / yolo;
+
+        System.out.print("Das Ergebnis von swag / yolo ist =");
+        System.out.println(ergebnis);
+
+
+    }
+}
---
+++ Source/src/com/company/Variablen.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Variablen {
+    public static void main(String [] args){
+        int swag;
+        int yolo = 5;
+        swag = 10;
+
+        System.out.println(swag);
+        System.out.println(yolo);
+
+        yolo = 6;
+        System.out.println(yolo);
+
+    }
+}
