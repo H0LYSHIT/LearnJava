f009815d221c249729881b811853ab31ca82df81
Pierre Michelsen <crakxhd@gmail.com>
2018-09-18 22:42:17 +0200

    Übung

---
+++ Source/src/com/company/PrimitiveDatentypen.java
@@ -0,0 +1,53 @@
+package com.company;
+
+public class PrimitiveDatentypen {
+    public static void main(String[] args) {
+
+        String name;
+
+        byte alter;
+
+        short groeße;
+
+        int einkommen;
+
+        double gewicht;
+
+        char geschlecht;
+
+        boolean istcool;
+
+        name = "Jan";
+
+        alter = 22;
+
+        einkommen = 1623;
+
+        groeße = 185;
+
+        gewicht = 85.;
+
+        geschlecht = 'm';
+
+        istcool = true;
+
+        System.out.println("Steckbrief");
+        System.out.println("Name: " + name);
+        System.out.println("Alter: " + alter);
+        System.out.println("Körpergröße: " + groeße);
+        System.out.println("Gehalt: " + einkommen);
+        System.out.println("Gewicht: " + gewicht);
+        System.out.println("Geschlecht: " + geschlecht);
+        System.out.println("Coole Sau? " + istcool);
+
+
+    }
+}
+
+
+
+
+
+
+
+
